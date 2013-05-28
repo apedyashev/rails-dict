7938b3ccfa9a621d6cc5b5a38d3fca3b845857c1
Alexey <alexey.pedyashev@gmail.com>
2013-05-28 20:03:42 +0200

    generated rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsDict::Application.config.secret_token = 'd28602a98ae4bb0cc30070fb7762d607852ed83fc78d074665fb8e1eac5eda9230be4ed32ae4d76e0e4a50e03fbff7db1141c7761b08cc88cbea5a11210089e8'
