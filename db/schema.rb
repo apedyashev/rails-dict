2b0c744ad29d8e6a8ff41c00d18fc6eeb404263e
apedyashev <alexey.pedyashev@gmail.com>
2013-09-05 23:37:34 +0400

    sd

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,11 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
+<<<<<<< HEAD
 ActiveRecord::Schema.define(:version => 20130905185401) do
+=======
+ActiveRecord::Schema.define(:version => 20130905193713) do
+>>>>>>> e5164fa... added activity log migration
 
   create_table "activity_logs", :force => true do |t|
     t.string   "action",     :limit => 15
